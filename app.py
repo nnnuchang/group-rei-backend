0d575fe3803ad09eeed2ed90f28f212b3ae10485
ycc2001 <ycc2001@gmail.com>
2023-09-05 17:45:56 +0800

    import item

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 from account import account
+from item import item
 
 app = Flask(__name__)
 
 app.register_blueprint(account, url_prefix = '/account')
+app.register_blueprint(item, url_prefix = '/item')
+
 
 @app.route('/')
 def index():
