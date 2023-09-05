b280f85b9e662f6fed46e2e84adda081a9e37f28
ChangCheWei <ericchang922@gmail.com>
2023-09-05 21:50:34 +0800

    Add timezone in login and make logout use "checkToken"

--- account.py
+++ account.py
@@ -4,8 +4,12 @@ import hashlib, os
 from datetime import datetime
 
 from dbConnection import dbConnect
+from checkToken import checkToken
+from logoutAll import logoutAll
 
 account = Blueprint('account',  __name__)
+
+timezone = 'Asia/Taipei'
 
 @account.route('/login', methods = ['POST'])
 def login():
@@ -16,22 +20,26 @@ def login():
     uid = request.get_json()['uid'].lower()
     password = hashlib.md5(request.get_json()['password'].encode('utf-8')).hexdigest()
 
+    try:
+        tzString = request.get_json()['timezone']
+    except:
+        tzString = 'UTC'
+
+    if tzString is None:
+        tzString = 'UTC'
+
     query = {"uid": uid}
     record = accountDb.find_one(query, {'_id': 0})
 
-    print({'uid': uid, 'password': password})
-    print(record)
-
     if record['password'] == password:
-        print('uid login')
         token = hashlib.sha1(os.urandom(24)).hexdigest()
 
         logoutAll(uid)
         loginDb.insert_one({
             'token_hash': hashlib.md5(token.encode('utf-8')).hexdigest(),
             'uid': uid,
-            'login_date': datetime.today().replace(microsecond = 0),
-            'is_login': True
+            'is_login': True,
+            'timezone': tzString
         })
 
         data = {
@@ -56,48 +64,19 @@ def login():
 
 @account.route('/logout', methods = ['POST'])
 def logout():
-    expiresDays = 30
-
-    loginDb = dbConnect('login_record')
-
     uid = request.get_json()['uid'].lower()
-    tokenHash = hashlib.md5(request.get_json()['token'].encode('utf-8')).hexdigest()
 
-    query = {
-        'uid': uid,
-        'token_hash': tokenHash,
-    }
+    tokenStatus = checkToken(uid, request.get_json()['token'], timezone)
 
-    record = loginDb.find_one(query, {"_id": 0})
-
-    if record is not None:
-        loginDays = (datetime.today()-record['login_date']).days
-
-        if loginDays > expiresDays:
-            data = {
-                'state': 'token expires',
-                'stateMessage': '此登入過期',
-                'date': record['login_date']
-            }
-            return jsonify(data)
-
-        if record['is_login'] == True:
-            logoutAll(uid)
-
-            data = {
-                'state': 'success',
-                'stateMessage': '登出成功'
-            }
-        else:
-            data = {
-                'state': 'token invalid',
-                'stateMessage': '此登入狀態無效'
-            }
+    if tokenStatus is not True:
+        data = tokenStatus
     else:
+        logoutAll(uid)
+
         data = {
-                'state': 'none login',
-                'stateMessage': '無此登入記錄'
-            }
+            'state': 'success',
+            'stateMessage': '登出成功'
+        }
 
     return jsonify(data)
 
@@ -158,10 +137,3 @@ def checkUid():
     
     return jsonify(data)
 
-
-# 登出所有裝置
-def logoutAll(uid):
-    loginDb = dbConnect('login_record')
-
-    allLoginData = loginDb.update_many({'uid': uid}, {'$set': {'is_login': False}})
-    print(uid, allLoginData.modified_count, "documents of login_record updated")
