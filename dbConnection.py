77bcd6825699b16221d3f270645ff03d1f677156
ycc2001 <ycc2001@gmail.com>
2023-09-05 17:45:35 +0800

    change url

--- dbConnection.py
+++ dbConnection.py
@@ -2,9 +2,12 @@ import pymongo
 
 def dbConnect(c):
     print( 'database connecting')
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client['testDB']
+    client = pymongo.MongoClient('mongodb://developer:A11123008@ericchang922.synology.me:27017/?authSource=group_rei')
+    db = client['group_rei']
+
+    
     col = db[str(c)]
+  
 
     return col
 
