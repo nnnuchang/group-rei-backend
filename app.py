0c65f4e6423812befaebd00fca469e201e984306
ycc2001 <ycc2001@gmail.com>
2023-10-09 16:32:49 +0800

    add chip blueprint

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 from account import account
 from item import item
+from chip import chip
 
 app = Flask(__name__)
 
 app.register_blueprint(account, url_prefix = '/account')
 app.register_blueprint(item, url_prefix = '/item')
+app.register_blueprint(chip, url_prefix = '/chip')
+
 
 
 @app.route('/')
