66d9bb33065d0aebdc6085dccff63856492da8d8
ChangCheWei <ericchang922@gmail.com>
2023-12-25 01:14:35 +0800

    Fix /chip/upgrade

--- chip.py
+++ chip.py
@@ -1,10 +1,13 @@
-from bson import ObjectId
 from flask import Blueprint, request, jsonify
-import hashlib, os
+
+from bson import ObjectId
+
 from public.dbConnection import dbConnect
+from public.checkToken import checkToken
 
 chip = Blueprint('chip',  __name__)
 
+timezone = 'Asia/Taipei'
 language = 'zh_tw'
 
 
@@ -47,36 +50,115 @@ def info():
 @chip.route('upgrade', methods = ['POST'])
 def upgrade():
     requestData = request.get_json()
+
     token = requestData['token']
-    objItemOid = requestData['objItemOid']
-    typeId = requestData['typeId']
-    u = requestData['u']
+    objItemOid = ObjectId(requestData['objItemOid'])
+    U = requestData['U']
     mu = requestData['mu']
 
-    bagDb = dbConnect('bag')
-    obj_itemDb = dbConnect('obj_item')
-    chipDb = dbConnect('chip')
+    tokenStatus = checkToken(token, timezone)
 
-    r = obj_itemDb.find_one({"_id":ObjectId(objItemOid)})
-
-    print(r)
-
-    if u[0]['UType'] == 'i00010':
-        exp = u[0]['UNum']*100
-    elif u[0]['UType'] == 'i00011':
-        exp = u[0]['UNum']*1000
+    if tokenStatus['status'] is not True:
+        data = tokenStatus
+        return jsonify(data)
     else:
-        exp = u[0]['UNum']*10000
-    print(exp)
+        uid = tokenStatus['uid']
 
-    obj_itemDb.update_one({"_id":ObjectId(objItemOid)},{'$inc':{'exp': exp}})
-    cost = exp*5
-    bagDb.update_one({"obj_items":ObjectId(objItemOid),'items.item_id':'i0001'},{'$inc':{'items.1.amount': -cost}})
-    afterexp = obj_itemDb.find_one({"_id":ObjectId(objItemOid)},{"exp":1,"_id":0})
-    cost_mu = bagDb.find_one({"obj_items":ObjectId(objItemOid),'items.item_id':'i0001'},{'items.amount':1, '_id':0})
-    print(cost_mu)
-    data = [cost_mu,afterexp]
-    print(data)
+    objItemDb = dbConnect('obj_item')
+    bagDb = dbConnect('bag')
+
+    accountDb = dbConnect('account')
+    commanderDb = dbConnect('commander')
+
+    itemDb = dbConnect('items')
+    chipDb = dbConnect('chip')
+    chipLevelDb = dbConnect('chip_level')
+
+    accountOid = accountDb.find_one({'uid': uid})['_id']
+    objItemRecord = objItemDb.find_one({'_id': objItemOid})
+
+    commanderRecord = commanderDb.find_one({'account_oid': accountOid})
+
+    itemId = objItemRecord['item_id']
+    oldExp = objItemRecord['exp']
+
+    try:
+        serverMu = int(commanderRecord['mu'])
+        havingMu = int(mu)
+
+        if serverMu!= havingMu:
+            print('m: {}\n'.format(serverMu))
+            return jsonify({
+                'statusCode': 403,
+                'failedMsg': '數值驗證失敗(konpeito)'
+            })
+    except:
+        return jsonify({
+                'statusCode': 400,
+                'failedMsg': '資料型態錯誤(konpeito)'
+            })
+
+    # 確認是否擁有道具
+    objItemsList = bagDb.find_one({'account_oid': accountOid})['obj_items']
+    print(objItemsList)
+
+    if objItemOid not in objItemsList:
+        return jsonify({
+            'statusCode': 404,
+            'failedMsg': '晶片不存在或不屬於使用者'
+        })
+
+    # 計算經驗、扣除素材
+    UType = {
+        'i0010': 100, # u238
+        'i0011': 1000, # u235
+        'i0012': 10000 # u234
+    }
+
+    upExp = 0
+    cost = 0
+    for i in U:
+        # 計算經驗
+        category = i['UType']
+        num = i['UNum']
+        exp = UType[category] * int(num)
+
+        upExp += exp
+
+        # 計算花費
+        cost += exp * 10
+        if (serverMu - cost) < 0:
+            print('mu not enough')
+            break
+
+        # 扣除素材
+        bagDb.update_one(
+            {'account_oid': accountOid, 'items.item_id': category},
+            {'$inc': {'items.$.amount': -num}}
+        )
+
+    # 確認經驗上限
+    itemOid = itemDb.find_one({'item_id': itemId})['_id']
+    chipLevel = chipDb.find_one({'item_oid': itemOid})['chip_level_id']
+    maxExp = chipLevelDb.find_one({'chip_level_id': chipLevel})['max_experience']
+
+    if oldExp + upExp > maxExp:
+        upExp = maxExp - oldExp
+
+    # 更新經驗
+    objItemDb.update_one({'_id': objItemOid}, {'$inc': {'exp': upExp}})
+
+    # 扣除立日
+    commanderDb.update_one({'account_oid': accountOid}, {'$inc': {'mu': -cost}})
+
+    newMu = dbConnect('commander').find_one({'account_oid': accountOid})['mu']
+    newExp = dbConnect('obj_item').find_one({'_id': objItemOid})['exp']
+
+    data = {
+        'mu': newMu,
+        'expAfter': newExp
+    }
+
     return jsonify(data)
 
 
